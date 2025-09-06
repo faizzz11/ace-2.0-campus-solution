880731664803bb7b28fb8d7f58c76707c84cdba3
faizzz11 <faizmoulavi54@gmail.com>
2025-09-07 01:12:45 +0530

    changed the routes, added the sign up page

--- app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SharedLayout from "@/components/shared-layout"
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <SharedLayout>{children}</SharedLayout>
-}
